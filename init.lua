bf402665be50746c02e865a494e21501d57b825b
Luc Billaud <luc.billaud@creatis.insa-lyon.fr>
2024-02-02 14:35:50 +0100

    Adding Indent blankline

--- init.lua
+++ init.lua
@@ -4,44 +4,45 @@ require("billuc.lazy")
 require("billuc.set")
 
 require("lazy").setup({
-        {
-                'nvim-telescope/telescope.nvim', tag = '0.1.5',
-                dependencies = { 'nvim-lua/plenary.nvim' }
-        },
-        { "rose-pine/neovim", name = "rose-pine" },
-        {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-        {
-                "theprimeagen/harpoon",
-                branch = "harpoon2",
-                dependencies = { "nvim-lua/plenary.nvim" }
-        },
-        { 'mbbill/undotree' },
-        { 'tpope/vim-fugitive' },
-        {
-                'VonHeikemen/lsp-zero.nvim', branch = 'v3.x',
-                dependencies = {
-                        { 'neovim/nvim-lspconfig' },
-                        {'hrsh7th/cmp-nvim-lsp'},
-                        {'hrsh7th/nvim-cmp'},
-                        {'L3MON4D3/LuaSnip'},
-                        {'williamboman/mason.nvim'},
-                        {'williamboman/mason-lspconfig.nvim'},
-                }
-        },
-        {'akinsho/toggleterm.nvim', version = "*", config = true},
-        {
-                "rbong/vim-flog",
-                lazy = true,
-                cmd = { "Flog", "Flogsplit", "Floggit" },
-        },
-        { 'tpope/vim-commentary' },
-        {
-                "folke/trouble.nvim",
-                dependencies = { "nvim-tree/nvim-web-devicons" },
-                opts = {
-                        position = "right",
-                        width = 50,
-                }
-        },
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    { "rose-pine/neovim", name = "rose-pine" },
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    {
+        "theprimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" }
+    },
+    { 'mbbill/undotree' },
+    { 'tpope/vim-fugitive' },
+    {
+        'VonHeikemen/lsp-zero.nvim', branch = 'v3.x',
+        dependencies = {
+            { 'neovim/nvim-lspconfig' },
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/nvim-cmp'},
+            {'L3MON4D3/LuaSnip'},
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+        }
+    },
+    {'akinsho/toggleterm.nvim', version = "*", config = true},
+    {
+        "rbong/vim-flog",
+        lazy = true,
+        cmd = { "Flog", "Flogsplit", "Floggit" },
+    },
+    { 'tpope/vim-commentary' },
+    {
+        "folke/trouble.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        opts = {
+            position = "right",
+            width = 50,
+        }
+    },
+    { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
 })
 
