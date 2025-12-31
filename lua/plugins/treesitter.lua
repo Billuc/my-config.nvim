33951b5de45c654353a62de5c53e14a3fb32ff91
Billuc <billuc@hotmail.fr>
2025-12-31 16:57:59 +0100

    Added statusline mode for terminal & enabled treesitter highlight and indent

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,4 +1,12 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate"
+    build = ":TSUpdate",
+    opts = {
+        highlight = {
+            enable = true
+        },
+        indent = {
+            enable = true
+        }
+    }
 }
